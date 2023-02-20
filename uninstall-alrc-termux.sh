f0194801d75e3b3c5578a3a45ca993d3e1eac74d
Luis Adha <40796508+luisadha@users.noreply.github.com>
2023-02-21 01:15:21 +0700

    Create uninstall-alrc-termux.sh

---
+++ uninstall-alrc-termux.sh
@@ -0,0 +1,7 @@
+rm -f ~/.local/bin/alrc-termux.sh
+rm -f ~/.local/bin/answer.txt
+rm -f ~/.local/bin/brandomusic-cache-clear.sh
+rm -f ~/.local/bin/Changelog.al.txt
+sleep 1
+echo 'Uninstall Completed!'
+login
